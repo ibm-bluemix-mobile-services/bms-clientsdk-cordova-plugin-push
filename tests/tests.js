9e316862e7dff697a3a8549f911d159199bdce5f
Sergio Escoto <escotos@us.ibm.com>
2015-10-23 14:01:59 -0500

    Began adding unit tests for MFPPush's API

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,45 @@
 exports.defineAutoTests = function() {
 	describe('MFPPush test suite', function () {
 
-	}));
-}
+		var fail = function (done, context, message) {
+            if (context) {
+                if (context.done) return;
+                context.done = true;
+            }
+
+            if (message) {
+                expect(false).toBe(true, message);
+            } else {
+                expect(false).toBe(true);
+            }
+            setTimeout(function () {
+                done();
+            });
+    	};
+        var succeed = function (done, context) {
+            if (context) {
+                if (context.done) return;
+                context.done = true;
+            }
+
+            expect(true).toBe(true);
+
+            setTimeout(function () {
+                done();
+            });
+        };
+
+		describe('MFPPush API', function() {
+
+			it('MFPPush should exist', function(){
+				expect(MFPPush).toBeDefined();
+			});
+
+			it('MFPPush.getSubscriptionStatus() should exist and is a function', function(){
+				expect(typeof MFPPush.getSubscriptionStatus).toBeDefined();
+				expect(typeof MFPPush.getSubscriptionStatus == 'function').toBe(true);
+			});
+
+		});
+	});
+};
--- www/MFPPush.js
+++ www/MFPPush.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "MFPPush", "coolMethod", [arg0]);
+var MFPPush = {
+	getSubscriptionStatus: function (){}
 };
+
+module.exports = MFPPush;
