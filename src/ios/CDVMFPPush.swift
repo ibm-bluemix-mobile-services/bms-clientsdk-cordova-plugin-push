7581c0ee3ed40b728f0b4941aca7cd2d6c2c82da
Matthew Parangot <mparang@us.ibm.com>
2015-10-27 00:24:03 -0500

    Implemented native ios code for getSubscriptionStatus and retrieveAvailableTags

---
+++ src/ios/CDVMFPPush.swift
@@ -0,0 +1,42 @@
+/*
+Copyright 2015 IBM Corp.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Foundation
+import IMFCore
+import IMFPush
+
+@objc(CDVMFPPush) class CDVMFPPush : CDVPlugin {
+    
+    let push = IMFPushClient.sharedInstance()
+    
+    func getSubscriptionStatus(command: CDVInvokedUrlCommand) {
+        
+        self.commandDelegate!.runInBackground({
+            let str = "got to native"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: str)
+            // call success callback
+            self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        })
+    }
+    
+    func retrieveAvailableTags(command: CDVInvokedUrlCommand) {
+        self.commandDelegate!.runInBackground({
+            self.push.retrieveAvailableTagsWithCompletionHandler { (response:IMFResponse!, error:NSError!) -> Void in
+                let tags = response.availableTags()
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArray: tags)
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                print(tags)
+            }
+        })
+    }
+}
